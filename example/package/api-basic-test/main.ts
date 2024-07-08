f59816d7165063692315e4054e28859d950594ea
buebos <98134814+buebos@users.noreply.github.com>
2024-07-07 17:13:53 -0700

    Fix (index)!: Updated exports

--- example/package/api-basic-test/main.ts
+++ example/package/api-basic-test/main.ts
@@ -1,5 +1,6 @@
 import { template } from "jaslog";
 
+const logbp = template.blueprint.terminal;
 const logger = template.chained.terminal;
 
 logger
@@ -9,3 +10,16 @@ logger
     .line()
     .desc("This is a message from the example/package/api-basic-test module")
     .desc("testing jaslog npm package.");
+
+logbp.line();
+logbp.line();
+
+logbp.error({
+    title: "Dependency manager missing.",
+    desc:
+        "Your system does not have any of the supported dependency managers " +
+        "installed. The create-next-kit requires at least one of the " +
+        "following dependency managers to function properly. Please install " +
+        "one of the following based on your project's dependency " +
+        "requirements:",
+});
--- index.ts
+++ index.ts
@@ -1,14 +1,13 @@
 export * from "./src/core/Format";
 export * from "./src/core/Logger";
 export * from "./src/core/Target";
-export * from "./src/provider/Level/base";
 export * from "./src/provider/Logger/Blueprint";
 export * from "./src/provider/Logger/Chained";
-export * from "./src/provider/Target/File";
 export * from "./src/provider/Target/Terminal";
+export * from "./src/template/base";
 
+import { chained, blueprint } from "./src/template/logger";
 import { terminal } from "./src/template/target";
-import { chained } from "./src/template/logger";
 
-export const template = { chained };
+export const template = { chained, blueprint };
 export const target = { terminal };
