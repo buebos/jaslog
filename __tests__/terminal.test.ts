49023839e55e626f5051c96e2f5fd0d54dcd978c
buebos <98134814+buebos@users.noreply.github.com>
2024-07-08 12:27:46 -0700

    Refactored terminal class to improve configurability
    Desc: Wrap can now be turned on or off from methods. Also the cursor
    limit and wrap mode are independent from it's on and off state so the
    instance stays configured and the user can turn off and on as it likes
    without losing state.
    
    Breaking: false
    Relevance: 70
    Tags: feat, chore, refactor, fix

---
+++ __tests__/terminal.test.ts
@@ -0,0 +1,22 @@
+import Terminal from "../src/provider/Target/Terminal";
+
+const MESSAGE =
+    "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas Letraset, las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.";
+
+const terminal = new Terminal({ wrap: "word", cursor: { limit: 60 } });
+
+terminal.write(MESSAGE);
+terminal.setWrapMode("char");
+terminal.newLine();
+terminal.newLine();
+terminal.write(MESSAGE);
+terminal.wrapOff();
+terminal.newLine();
+terminal.newLine();
+terminal.write(MESSAGE);
+
+terminal.wrapOn();
+terminal.setCursorLimit(200);
+terminal.newLine();
+terminal.newLine();
+terminal.write(MESSAGE);
--- src/provider/Target/Terminal.ts
+++ src/provider/Target/Terminal.ts
@@ -1,5 +1,5 @@
-import Format from "../../core/Format";
-import { Target } from "../../core/Target";
+import { Format } from "../../core/Format";
+import Target from "../../core/Target";
 import {
     ANSI_BOLD,
     ANSI_COLOR_POSTFIX,
@@ -7,28 +7,52 @@ import {
     ANSI_RESET,
 } from "../../util/ansi/constant";
 
+type TerminalWrap = "word" | "char";
+
 type Config = {
+    wrap?: TerminalWrap | "off";
     cursor?: {
         limit?: number;
     };
 };
 
-const TERMINAL_CURSOR_NO_LIMIT = -1;
-const TERMINAL_CURSOR_MAX = 120;
+const TERMINAL_CURSOR_LIMIT = 120;
 
 class Terminal implements Target {
-    private wrap: "word" | "char" = "word";
+    private wrap: TerminalWrap = "word";
     private cursor = {
         current: 0,
-        limit: TERMINAL_CURSOR_MAX,
+        limit: TERMINAL_CURSOR_LIMIT,
     };
+
+    private shouldWrap: boolean = true;
 
     constructor(config?: Config) {
         if (config?.cursor?.limit === 0) {
             throw new Error("Invalid terminal cursor max width");
         }
 
+        if (config?.wrap == "off") {
+            this.shouldWrap = false;
+            return;
+        }
+
         this.cursor.limit = config?.cursor?.limit ?? this.cursor.limit;
+        this.wrap = config?.wrap ?? this.wrap;
+    }
+
+    wrapOn() {
+        this.shouldWrap = true;
+    }
+    wrapOff() {
+        this.shouldWrap = false;
+    }
+
+    setWrapMode(mode: TerminalWrap) {
+        this.wrap = mode;
+    }
+    setCursorLimit(limit: number) {
+        this.cursor.limit = limit;
     }
 
     newLine(): void {
@@ -37,10 +61,17 @@ class Terminal implements Target {
     }
 
     write(data: string): void {
-        if (this.wrap === "word") {
-            this.writeByWord(data);
-        } else {
-            this.writeByChar(data);
+        switch (this.wrap) {
+            case "word": {
+                this.writeByWord(data);
+                break;
+            }
+            case "char": {
+                this.writeByChar(data);
+                break;
+            }
+            default:
+                break;
         }
     }
 
@@ -50,28 +81,31 @@ class Terminal implements Target {
 
         while (index < length) {
             let nextSpace = data.indexOf(" ", index);
-            if (nextSpace === -1) nextSpace = length;
+
+            if (nextSpace === -1) {
+                nextSpace = length;
+            }
 
             const word = data.slice(index, nextSpace);
-            const wordLength = word.length;
             const remainingSpace = this.cursor.limit - this.cursor.current;
 
-            if (
-                this.cursor.limit > TERMINAL_CURSOR_NO_LIMIT &&
-                wordLength > remainingSpace
-            ) {
+            if (this.shouldWrap && word.length > remainingSpace) {
                 this.newLine();
             }
 
+            /**
+             * If the word length is still greater than the cursor limit just
+             * print it. This would mean that the cursor limit is small.
+             */
             process.stdout.write(word);
-            this.cursor.current += wordLength;
+            this.cursor.current += word.length;
 
             if (nextSpace < length) {
                 process.stdout.write(" ");
                 this.cursor.current += 1;
             }
 
-            if (this.cursor.current >= this.cursor.limit) {
+            if (this.shouldWrap && this.cursor.current >= this.cursor.limit) {
                 this.newLine();
             }
 
@@ -82,10 +116,11 @@ class Terminal implements Target {
     private writeByChar(data: string): void {
         for (let i = 0; i < data.length; i++) {
             const char = data[i];
+
             process.stdout.write(char);
             this.cursor.current += 1;
 
-            if (this.cursor.current >= this.cursor.limit) {
+            if (this.shouldWrap && this.cursor.current >= this.cursor.limit) {
                 this.newLine();
             }
         }
